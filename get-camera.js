82df757ed4be69a39bbdf19f5d01c1bffa2a8159
Zohaib Khan Jadoon <149315703+Zohaib-Jadoon@users.noreply.github.com>
2026-06-01 17:37:05 +0500

    build(cleanup): remove unnecessary temporary test and scratch files from repository

--- get-camera.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('./apps/backend/node_modules/@prisma/client');
-const prisma = new PrismaClient();
-prisma.camera.findFirst().then(camera => {
-  if (camera) {
-    console.log(camera.id);
-  } else {
-    console.log('No camera');
-  }
-}).finally(() => prisma.$disconnect());
